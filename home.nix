9be592073ac5b86fd7bccef855a82d54d0e62136
kirsinger <kai.hirsinger@gmail.com>
2026-09-07 14:41:58 +1000

    fix symlink to CLAUDE.md

--- home.nix
+++ home.nix
@@ -99,6 +99,6 @@ in
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.claude/settings.json";
   home.file."projects/mise.toml".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/global.mise.toml";
-  home.file."projects/AGENTS.md".source =
+  home.file.".claude/CLAUDE.md".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/AGENTS.md";
 }
